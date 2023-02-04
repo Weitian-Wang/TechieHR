089d41dca040657790981771940824febf7b2001
wangweitian <weitianwang624@gmail.com>
2023-02-04 15:35:54 -0800

    frontend register redirect url bug fix

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -61,7 +61,7 @@ const Login = () => {
 				</div>
 				<div className={styles.right}>
 					<h1>New Here ?</h1>
-					<Link to="/signup">
+					<Link to="/register">
 						<button type="button" className={styles.white_btn}>
                             Sign Up
 						</button>
